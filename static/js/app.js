e64586f5d05a6ec559ee9ae61e847119d24fac74
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-MacBook-Pro.local>
2021-03-11 22:43:11 -0500

    Listenner & Hook filter-btn

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+// D3.js, to equip our website to "listen" for events, such as a user clicking a button.
 // import the data from data.js
 const tableData = data;
 // Reference the HTML table using d3
@@ -12,6 +13,7 @@ function buildTable(data) {
 
   // Next, loop through each object in the data
   // and append a row and cells for each value in the row
+  // By starting our line of code with Object.values, we're telling JavaScript to reference one object from the array of UFO sightings. By adding (dataRow
   data.forEach((dataRow) => {
     // Append a row to the table body
     let row = tbody.append("tr");
@@ -24,3 +26,28 @@ function buildTable(data) {
     });
   });
 }
+
+function handleClick() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+  // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter((row) => row.datetime === date);
+  }
+
+  // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
